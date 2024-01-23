3f5fca099012e43d793db0eff0233bb2e2d4a135
Asilbek <www.gubernatorrr@gmail.com>
2024-01-23 20:12:35 +0500

    ...

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import home_page, article_page, article_detail_page, about_page
 
 urlpatterns = [
-    path('', home_page, name='home'),
+    path('', home_page, name='Home'),
     path('article/', article_page, name='article'),
     path('article/<int:pk>/', article_detail_page, name='detail'),
     path('about/', about_page, name='about')
